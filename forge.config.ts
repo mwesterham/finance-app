94e891a5bf7c74b3e7f128ca069af70641afa03e
mwesterham <56182492+mwesterham@users.noreply.github.com>
2025-06-29 09:26:00 -0700

    Only build for platform which is supported

--- forge.config.ts
+++ forge.config.ts
@@ -2,6 +2,7 @@ import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
 import { MakerDeb } from '@electron-forge/maker-deb';
+import { MakerRpm } from '@electron-forge/maker-rpm';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
@@ -10,12 +11,22 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
 
+import os from 'os';
+
+const isWindows = process.platform === 'win32';
+const isMac = process.platform === 'darwin';
+const isLinux = process.platform === 'linux';
+
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
   },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerDeb({})],
+  makers: [
+    ...(isWindows ? [new MakerSquirrel({})] : []),
+    ...(isMac ? [new MakerZIP({}, ['darwin'])] : []),
+    ...(isLinux ? [new MakerDeb({}), new MakerRpm({})] : [])
+  ],
   publishers: [
     {
       name: '@electron-forge/publisher-github',
